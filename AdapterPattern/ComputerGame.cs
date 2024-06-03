dfc1a0d49557f555a5b94c81de0a40ce146b3577
MillerKV <MillerKV@201D-2>
2024-06-03 14:12:31 +0700

    Добавьте файлы проекта.

---
+++ AdapterPattern/ComputerGame.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    public class ComputerGame
+    {
+        private string name;
+        private PegiAgeRating pegiAgeRating;
+        private double budgetInMillionsOfDollars;
+        private int minimumGpuMemoryInMegabytes;
+        private int diskSpaceNeededInGB;
+        private int ramNeededInGb;
+        private int coresNeeded;
+        private double coreSpeedInGhz;
+        private string v1;
+        private PegiAgeRating p18;
+        private int v2;
+        private int v3;
+        private int v4;
+        private double v5;
+        private int v6;
+
+        public ComputerGame(string name,
+                            PegiAgeRating pegiAgeRating,
+                            double budgetInMillionsOfDollars,
+                            int minimumGpuMemoryInMegabytes,
+                            int diskSpaceNeededInGB,
+                            int ramNeededInGb,
+                            int coresNeeded,
+                            double coreSpeedInGhz)
+        {
+            this.name = name;
+            this.pegiAgeRating = pegiAgeRating;
+            this.budgetInMillionsOfDollars = budgetInMillionsOfDollars;
+            this.minimumGpuMemoryInMegabytes = minimumGpuMemoryInMegabytes;
+            this.diskSpaceNeededInGB = diskSpaceNeededInGB;
+            this.ramNeededInGb = ramNeededInGb;
+            this.coresNeeded = coresNeeded;
+            this.coreSpeedInGhz = coreSpeedInGhz;
+        }
+
+        public ComputerGame(string v1, PegiAgeRating p18, int v2, int v3, int v4, double v5, int v6)
+        {
+            this.v1 = v1;
+            this.p18 = p18;
+            this.v2 = v2;
+            this.v3 = v3;
+            this.v4 = v4;
+            this.v5 = v5;
+            this.v6 = v6;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public PegiAgeRating getPegiAgeRating()
+        {
+            return pegiAgeRating;
+        }
+
+        public double getBudgetInMillionsOfDollars()
+        {
+            return budgetInMillionsOfDollars;
+        }
+
+        public int getMinimumGpuMemoryInMegabytes()
+        {
+            return minimumGpuMemoryInMegabytes;
+        }
+
+        public int getDiskSpaceNeededInGB()
+        {
+            return diskSpaceNeededInGB;
+        }
+
+        public int getRamNeededInGb()
+        {
+            return ramNeededInGb;
+        }
+
+        public int getCoresNeeded()
+        {
+            return coresNeeded;
+        }
+
+        public double getCoreSpeedInGhz()
+        {
+            return coreSpeedInGhz;
+        }
+    }
+
+    public enum PegiAgeRating
+    {
+        P3, P7, P12, P16, P18
+    }
+
+    public class Requirements
+    {
+        private int gpuGb;
+        private int HDDGb;
+        private int RAMGb;
+        private double cpuGhz;
+        private int coresNum;
+
+        public Requirements(int gpuGb,
+                            int HDDGb,
+                            int RAMGb,
+                            double cpuGhz,
+                            int coresNum)
+        {
+            this.gpuGb = gpuGb;
+            this.HDDGb = HDDGb;
+            this.RAMGb = RAMGb;
+            this.cpuGhz = cpuGhz;
+            this.coresNum = coresNum;
+        }
+
+        public int getGpuGb()
+        {
+            return gpuGb;
+        }
+
+        public int getHDDGb()
+        {
+            return HDDGb;
+        }
+
+        public int getRAMGb()
+        {
+            return RAMGb;
+        }
+
+        public double getCpuGhz()
+        {
+            return cpuGhz;
+        }
+
+        public int getCoresNum()
+        {
+            return coresNum;
+        }
+    }
+}
---
+++ AdapterPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using AdapterPattern;
+
+ComputerGame computerGame = new ComputerGame("Cool Game", PegiAgeRating.P16, 60, 2048, 50, 16, 8, 3.2);
+
+
+ComputerGameAdapter adapter = new ComputerGameAdapter(computerGame);
+
+
+Console.WriteLine("Title: " + adapter.getTitle());
+Console.WriteLine("PEGI Allowed Age: " + adapter.getPegiAllowedAge());
+Console.WriteLine("Is Triple-A Game: " + (adapter.isTripleAGame() ? "Yes" : "No"));
+
+Requirements gameRequirements = adapter.getRequirements();
+Console.WriteLine("Minimum GPU Memory: " + gameRequirements.getGpuGb() + " GB");
+Console.WriteLine("Disk Space Needed: " + gameRequirements.getHDDGb() + " GB");
+Console.WriteLine("RAM Needed: " + gameRequirements.getRAMGb() + " GB");
+Console.WriteLine("Core Speed: " + gameRequirements.getCpuGhz() + " GHz");
+Console.WriteLine("Number of Cores Needed: " + gameRequirements.getCoresNum() + ".");
+
